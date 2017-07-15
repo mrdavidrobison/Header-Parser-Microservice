d167521ae7e5410bc02a6dd6de7f69c08587e564
david robison <mrdavidrobison@gmail.com>
2017-07-15 12:46:54 -0600

    instantiate express

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+// required imports
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+
+// instantiate express for the app and bodyParser and Cors
+
+var app = module.exports = express();
+app.use(bodyParser.json());
+app.use(cors);
