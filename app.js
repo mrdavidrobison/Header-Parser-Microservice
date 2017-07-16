994b1bb7afd558a4df1ca30deceb0b7e280e3eb3
david robison <mrdavidrobison@gmail.com>
2017-07-16 09:48:22 -0600

    add api and get function

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 // required imports
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
 // instantiate express for the app and bodyParser and Cors
-
 var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors);
+
+// url
+var api = '/api/whoami';
+
+app.get(api, function(req, res, next){
+  var language;
+  var software;
+  var ipaddress = req.ip;
+  res.json({'ipaddress': ipaddress});
+});
 
 app.listen(3000, function(){
   console.log('working');
