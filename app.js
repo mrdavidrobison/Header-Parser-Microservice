ab567f62a1bc8525b84ddf4395208f4dd593002a
david robison <mrdavidrobison@gmail.com>
2017-07-20 09:48:23 -0600

    add software heading, include express-useragent

--- app.js
+++ app.js
@@ -2,20 +2,23 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var userAgent = require('express-useragent')
+
 
 // instantiate express for the app and bodyParser and Cors
 var app = module.exports = express();
 app.use(bodyParser.json());
-app.use(cors);
+app.use(cors());
+app.use(useragent.express());
 
-// url
+// api url
 var api = '/api/whoami';
 
 app.get(api, function(req, res, next){
-  var language = req.acceptsLanguages;
-  var software;
+  var language = req.acceptsLanguages();
+  var software = req.useragent;
   var ipaddress = req.ip;
-  res.json({'ipaddress': ipaddress, 'language': language});
+  res.json({'ipaddress': ipaddress, 'language': language[0], 'software': software});
 });
 
 app.listen(3000, function(){
